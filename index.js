46eb84447a78a780ec82819c6597d6e22f3e7498
Patrick Kim <pkim050@gmail.com>
2019-06-06 23:19:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,12 +50,8 @@ class Passenger {
 class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId;
-    if (driver) {
-      this.driverId = driver.id;
-    }
-    if (passenger) {
-      this.passengerId = passenger.id;
-    }
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
     store.trips.push(this);
   }
   driver() {
