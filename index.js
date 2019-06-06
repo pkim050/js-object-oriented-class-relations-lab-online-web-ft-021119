79684220ffa92a1ceb9218168b92fe4b4804b76b
Patrick Kim <pkim050@gmail.com>
2019-06-06 23:13:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let store = {drivers: [], trips: [], passengers: []}
 let driverId = 0;
-let tripsId = 0;
+let tripId = 0;
 let passengersId = 0;
 
 class Driver {
@@ -17,6 +17,65 @@ class Driver {
     );
   }
   passengers() {
-    return
+    return store.passengers.filter(
+      function(passenger) {
+        return passenger.driverId === this.id;
+      }.bind(this)
+    );
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this);
+  }
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId === this.id;
+      }.bind(this)
+    );
+  }
+  drivers() {
+    return store.drivers.filter(
+      function(driver) {
+        return driver.passengerId === this.id;
+      }.bind(this)
+    );
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    if (driver) {
+      this.driverId = driver.id;
+    }
+    if (passenger) {
+      this.passengerId = passenger.id;
+    }
+    store.trips.push(this);
+  }
+  setDriver(driver) {
+    this.driverId = driver.id;
+  }
+  setPassenger(passenger) {
+    this.passengerId = passenger.id;
+  }
+  driver() {
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
   }
 }
